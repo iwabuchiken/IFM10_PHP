ba73d8c1b2b95072e0e9da5dd17a8e2922e4d18b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-11-26 20:35:05 +0900

    J/C

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	//http://phpspot.net/php/pg%E3%83%87%E3%82%A3%E3%83%AC%E3%82%AF%E3%83%88%E3%83%AA%E3%81%AE%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%83%AA%E3%82%B9%E3%83%88%E3%82%92%E5%8F%96%E5%BE%97.html
-	
+
+
 // 	if ($dir = opendir("data/")) {
 	if ($dir = opendir(".")) {
 		
